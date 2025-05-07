1b24db040e373e776f72d986ad82420ed1bbf07e
Naiyyar <naiyyar.abbas512013@gmail.com>
2025-05-07 08:02:21 +0530

    sending boards and lists in json + typo fixes

--- app/controllers/api/v1/boards_controller.rb
+++ app/controllers/api/v1/boards_controller.rb
@@ -2,37 +2,37 @@ class Api::V1::BoardsController < ApplicationController
   before_action :set_board, only: [:show, :update, :destroy]
 
   def index
-    render json: {data: Board.all }
+    render json: { data: current_user.boards.rank(:row_order) }
   end
 
   def show
     switch_board(@board)
-    render json: { data: { board: @board, lists: @board.lists } }
+    render json: { data: board_data }
   end
 
   def create
-    board = current_user.boards.new(board_params)
-    if board.save
-      switch_board(board)
-      render json: {message: 'Board created successfully', data: board }
+    @board = current_user.boards.new(board_params)
+    if @board.save
+      switch_board(@board)
+      render json: {message: 'Board created successfully', data: board_data }
     else
-      render json: {message: 'Error in creating board', error: board.errors }
+      render json: {message: 'Error in creating board', error: @board.errors }
     end
   end
 
   def update
     if @board.update(board_params)
-      render json: {message: 'Board successfully updated', data: board }
+      render json: {message: 'Board successfully updated', data: @board }
     else
-      render json: {message: 'Error in updated board', error: board.errors }
+      render json: {message: 'Error in updated board', error: @board.errors }
     end
   end
 
   def destroy
     if @board.destroy
-      render json: {message: 'Board successfully deleted', data: board }
+      render json: {message: 'Board successfully deleted', data: @board }
     else
-      render json: {message: 'Error in deleted board', error: board.errors }
+      render json: {message: 'Error in deleted board', error: @board.errors }
     end
   end
 
@@ -44,5 +44,9 @@ class Api::V1::BoardsController < ApplicationController
 
   def board_params
     params.require(:board).permit(:name, :user_id, :background, :row_order)
+  end
+
+  def board_data
+    { board: current_board, lists: current_board.lists.rank(:row_order) }
   end
 end
